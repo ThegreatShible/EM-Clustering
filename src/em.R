1c7a091541d18745ff9f621f6894fa88a2a271dc
camillefossier <camille.fossier@gmail.com>
2019-11-20 20:12:38 +0100

    Classify now takes categorial variables (needs testing)

--- src/em.R
+++ src/em.R
@@ -4,8 +4,10 @@ EM <- function(X, K, nb_init=10) {
   d = ncol(X)
   n = nrow(X)
   
+  # TODO : split X into Xc and Xq
+  
   theta = init_theta(d, k)
-  proba = classify(X, theta)
+  proba = classify(theta, Xc, Xq)
   
   # E-step
   # Store all the likelihoods in a N x k amtrix
@@ -37,22 +39,58 @@ init_theta <- function(d, k) {
 
 # Returns matrices of size n x k of probabilities
 # of each element belonging to each class
-classify <- function(X, theta) {
-  n = nrow(X)
-  d = ncol(X)
-  k = length(theta)
-  proba = apply(matrix(1:k, nrow=1), 2, function(i) mdnorm(X, theta[[i]]$mean, theta[[i]]$sd))
-  proba = t(apply(proba, 1, function(i) i / sum(i)))
+classify <- function(thetas, Xc=NULL, Xq=NULL) {
+  if (!is.null(Xc)) n = nrow(Xc)
+  else {
+    if (!is.null(Xq)) n = nrow(Xq)
+    else stop("Dataset is empty")
+  }
+  proba = apply(matrix(seq_along(thetas), nrow=1), 2, function(i) multinomial(Xq, thetas[[i]]$alpha) * thetas[[i]]$p * mdnorm(Xc, thetas[[i]]$mean, thetas[[i]]$sd))
+  
+  # TODO : To check, but in theory the following line can be deleted
+  # It was there to balance probabilities before I put the pk (thetas[[i]]$p) in the line above
+  
+  # proba = t(apply(proba, 1, function(i) i / sum(i)))
+  
   return(proba)
 }
 
 # Equivalent of function dnorm but takes as inputs
 # a vector of mean and a matrix of standard deviation
 mdnorm <- function(X, mean, sd) {
+  if (is.null(X)) return(1)
   X = matrix(X, ncol=length(mean))
   p = length(mean)
   inv_sd = solve(sd)
   a = ((2 * pi) ^ (p / 2)) * (det(sd) ^ (1/2))
   b = apply(X, 1, function(x) - (1/2) * (t(x-mean) %*% inv_sd %*% (x-mean)))
   return((1 / a) * exp(b))
+}
+
+multinomial <- function(X, alpha) {
+  # alpha est un vecteur de taille la somme des nombres de modalités de toutes les variables
+  if (is.null(X)) return(1)
+  
+  nb_modalities = sum(apply(X, 2, function(c) length(levels(factor(c)))))
+  len_alpha = length(alpha)
+  if (nb_modalities != len_alpha)
+    stop(paste(c("Number of modalities (", nb_modalities, ") is different from the number of values in alpha (", len_alpha, ")"), collapse=""))
+  
+  X_hot = NULL
+  p = ncol(X)
+  for (j in 1:p) {
+    X_hot = cbind(X_hot, one_hot(factor(X[,j])))
+  }
+  print(X_hot)
+  return(apply(X_hot, 1, function(i) prod(alpha ^ i)))
+}
+
+one_hot <- function(x) {
+  lvl = levels(x)
+  len = length(lvl)
+  t(apply(matrix(x, ncol=1), 1, function(i) {
+    res = rep(0,len)
+    res[which(lvl == i)] = 1
+    res
+  }))
 }
