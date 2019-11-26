5373343370114de30b761a695e464c5c1a3e6143
camillefossier <camille.fossier@gmail.com>
2019-11-26 21:00:53 +0100

    small changes on test

--- examples/test2_simple_clustering.R
+++ examples/test2_simple_clustering.R
@@ -7,11 +7,15 @@ b = c(rnorm(n, 0, 2), rnorm(n, 4, 0.5))
 X = as.data.frame(cbind(a, b))
 
 plot(X)
-nbClust = 2
-clusty = clust(X=X, nbClust=nbClust, models="VVV",  nbInit=10, initMethod="random", epsilon=0.1)
+
+K=4
+clusty = clust(X=X, nbClust=1:K, models="VVV",  nbInit=2, initMethod="random", epsilon=0.5)
+
 for( c in clusty[[1]]) {
   print(c$icl)
 }
 Z = clusty[[1]][[1]]$Z
 Z = cbind(Z, apply(Z, 1, which.max))
-plot(X[,1], X[,2], col=c("red", "green" ,"blue")[Z[,nbClust+1]])
+#plot(X[,1], X[,2], col=c("red", "green" ,"blue")[Z[,nbClust+1]])
+
+plot_result(clusty)
