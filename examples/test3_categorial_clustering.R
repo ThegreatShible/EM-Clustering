9d8d36edabfb17fc50d4bbbbc290d8b4ad75de22
Nabih <en_nebbache@esi.dz>
2019-12-01 18:38:31 +0100

    error removal

--- examples/test3_categorial_clustering.R
+++ examples/test3_categorial_clustering.R
@@ -7,7 +7,6 @@ a = c(rep(1, n), rep(2, n), rep(3, n))
 b = c(rnorm(n), rnorm(2*n, 4, 2))
 X = data.frame(cbind(a, b))
 X$a <- as.factor(X$a)
-split_by_var_type(Byar)
 K=2:8
 clusty = clust(X=X, nbClust=K,  nbInit=20, initMethod="kmeans", epsilon=0.1)
 plot_result(clusty)
